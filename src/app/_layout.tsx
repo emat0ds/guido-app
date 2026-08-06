046adb1fb9414a990dc120024ac76ad77c6c72bc
emat0ds <utilepersiti@gmail.com>
2026-08-06 21:29:20 +0200

    fix: rimuovi Sentry che causa crash build

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,11 @@
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import * as Sentry from '@sentry/react-native';
 import { useOnboarding } from '@/hooks/useOnboarding';
 import { requestNotificationPermissions, setupDailyReminder } from '@/lib/notifications';
 import { hasStudiedToday } from '@/lib/storage';
 import { BadgeProvider } from '@/contexts/BadgeContext';
 import { ThemeProvider, useTheme } from '@/contexts/ThemeContext';
-
-Sentry.init({
-  dsn: 'https://88d9b9464c4af9bc728bf5d7eb119dce@o4511750197870592.ingest.de.sentry.io/4511750229852240',
-  sendDefaultPii: true,
-  enableLogs: true,
-  tracesSampleRate: 0.2,
-  enableAutoSessionTracking: true,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
-  enabled: !__DEV__,
-});
 
 SplashScreen.preventAutoHideAsync();
 
@@ -78,4 +65,4 @@ function RootLayout() {
   );
 }
 
-export default Sentry.wrap(RootLayout);
+export default RootLayout;
